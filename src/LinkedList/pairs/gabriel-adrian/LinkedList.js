62fe95f9e8b66028ea6eee744fb5262e14c2397f
Adrian Prisacaru <adrianprisacaru@Adrians-MacBook-Pro.local>
2021-10-08 15:42:13 +0300

    Creation of LinkedList and Tests

---
+++ src/LinkedList/pairs/gabriel-adrian/LinkedList.js
@@ -0,0 +1,11 @@
+class LinkedList {
+    constructor(number = null) {
+        this.number = number;
+    }
+
+    addElement(number) {
+        return number;
+    }
+}
+
+module.exports = LinkedList;
--- tests/LinkedList/pairs/gabriel-adrian/LinkedList.test.js
+++ tests/LinkedList/pairs/gabriel-adrian/LinkedList.test.js
@@ -1,9 +1,16 @@
 const LinkedList = require('../../../../src/LinkedList/pairs/gabriel-adrian/LinkedList')
 
 describe('LinkedList', () => {
+
     test('can create a new list', () => {
         const list = new LinkedList()
-
         expect(list).toBeDefined()
     })
+
+    test('can I add an input?', () => {
+        const list = new LinkedList()
+        expect(list.addElement(100)).toEqual(100)
+    })
+
 })
+
